299267f1b0d1622b8b59736e0d3b2f9718b1431d
serebriakovanton <cu-hawk@yandex.ru>
2024-08-04 17:21:49 +0700

    create DRF_Project: User(from AbstractUser), Course, Lesson - models. Settings.py is ready to start app.

---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="email")
+    phone = models.CharField(max_length=100, verbose_name='phone', null=True, blank=True)
+    avatar = models.ImageField(upload_to='users/', verbose_name='avatar', null=True, blank=True)
+    country = models.CharField(max_length=150, verbose_name='country', null=True, blank=True)
+    token = models.CharField(max_length=100, verbose_name='token', null=True, blank=True)
+    is_active = models.BooleanField(default=True, verbose_name='is_active status',null=True, blank=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
